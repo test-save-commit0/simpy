9e4e41d344039521ed32db3ea27a534ac37d3e33
Peter Grayson <pete@jpgrayson.net>
2023-11-12 17:31:35 -0500

    test: enable ruff flake8-simplify lints

--- src/simpy/core.py
+++ src/simpy/core.py
@@ -222,11 +222,7 @@ class Environment:
             if not isinstance(until, Event):
                 # Assume that *until* is a number if it is not None and
                 # not an event.  Create a Timeout(until) in this case.
-                at: SimTime
-                if isinstance(until, int):
-                    at = until
-                else:
-                    at = float(until)
+                at: SimTime = until if isinstance(until, int) else float(until)
 
                 if at <= self.now:
                     raise ValueError(
